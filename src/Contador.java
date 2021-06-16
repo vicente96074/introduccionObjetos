65fea8e7751a1aa957fa60b74e23e4de33426b10
Augusto Vicente <augustovicente201831464@cunoc.edu.gt>
2021-06-15 23:18:08 -0600

    Terminar de codificar la clase Contador,java

--- src/Contador.java
+++ src/Contador.java
@@ -1,5 +1,49 @@
 package src;
 
 public class Contador{
-	
+
+	//Constructor por defecto
+	public Contador(){		
+	}
+
+	//Constructor con parametros
+	public Contador(int numero){
+		System.out.println("Ejercicio 1.");
+		this.numero = numero;
+		incremento(numero);
+		decremento(numero);
+		System.out.println("");
+	}
+
+
+	//Metodos para incrementar
+	public void incremento(int numero){
+			System.out.println("Incrementar desde 0 hasta " + numero);
+			for(int i=0; i<=numero; i++){
+				System.out.print(i + " ");
+			}
+	}
+
+	//Metodo para decrementar
+
+	public void decremento(int numero){
+			System.out.println("\nDecrementar desde " + numero + " hasta 0");
+			for(int i=numero; i>=0; i--){
+				System.out.print(i + " ");
+			}
+	}
+
+
+	//Metodos getters y setters
+	public void setNumero(int numero){
+		this.numero = numero;
+	}
+
+
+	public int getNumero(){
+		return this.numero;
+	}
+
+	//Variables respestando ocultamiento de datos
+	private int numero;
 }
--- src/Principal.java
+++ src/Principal.java
@@ -5,7 +5,9 @@ public class Principal{
 
 	//Metodo mian
 	public static void main(String[] args){
-		
+
+		//Instanciamos un objeto de la clase contador(crear objeto)
+		Contador contador = new Contador(10);
 		
 	}
 }
