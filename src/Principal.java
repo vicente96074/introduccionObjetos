503fa008107c6d49c8d9ab428431a39bbe65fe76
Augusto Vicente <augustovicente201831464@cunoc.edu.gt>
2021-06-16 11:47:57 -0600

    Terminar de codificar a la clase Cuadrado.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,13 +1,15 @@
 package src;
-
+import src.*;	
 
 public class Principal{
 
 	//Metodo mian
 	public static void main(String[] args){
 
-		//Instanciamos un objeto de la clase contador(crear objeto)
+		//Instanciamos un objeto
+		//De la clase contador(crear objeto)
 		Contador contador = new Contador(10);
-		
+		//De la clase Cuadrado
+		Cuadrado cuadrado = new Cuadrado(5);
 	}
 }
